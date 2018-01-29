284ea4410633718ab35ee1489f10abac26d206d4
k.blatsoukas <k.blatsoukas@jrtechnologies.com>
2018-01-29 14:37:54 +0200

    minor changes

--- src/main/java/kon/blats/rest/Client.java
+++ src/main/java/kon/blats/rest/Client.java
@@ -36,10 +36,10 @@ public class Client {
     public static void sendTransaction() throws IOException {
         try {
             CredentialsProvider provider = new BasicCredentialsProvider();
-            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("apiuser@ifg.com", "aFw92H@Fb1");
+            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("apiuser@ola.com", "kostas");
             provider.setCredentials(AuthScope.ANY, credentials);
             HttpClient client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
-            HttpPost httpPost = new HttpPost("http://35.190.25.70:80/v2/order");
+            HttpPost httpPost = new HttpPost("http://localhost:80/v5/nikos");
             String json = getJsonExample();
             StringEntity entity = new StringEntity(json, "application/json","UTF-8");
             httpPost.setEntity(entity);
@@ -54,118 +54,7 @@ public class Client {
 
     public static String getJsonExample() {
         return "{\n" +
-                "\"orders\": [\n" +
-                "    {\n" +
-                "    \"flightRecords\": [\n" +
-                "        {\n" +
-                "            \"routingDIIndicator\": \" \",\n" +
-                "            \"passengerRecord\": {\n" +
-                "                \"name\": \"TIWARI/VIRAJMR\",\n" +
-                "                \"typeCode\": \"CNN\",\n" +
-                "                \"PNR\": \"RFLLEU/F1\",\n" +
-                "                \"dateOfBirth\": \"20140102\"\n" +
-                "            },\n" +
-                "            \"documentID\": {\n" +
-                "                \"airlineCodeCRC\": \"3\",\n" +
-                "                \"ticketDocumentNumber\": \"0741827456000\",\n" +
-                "                \"ticketingAirlineCodeNumber\": \"074\",\n" +
-                "                \"ticketCRC\": \"2\",\n" +
-                "                \"ticketingModeIndicator\": \"/\",\n" +
-                "                \"couponUseIndicator\": \"FVVV\"\n" +
-                "            },\n" +
-                "            \"documentDetails\": {\n" +
-                "                \"emdDetails\": [\n" +
-                "                    {\n" +
-                "                        \"couponNumber\": \"1\",\n" +
-                "                        \"couponValue\": \"00000002500\",\n" +
-                "                        \"currency\": \"EUR2\",\n" +
-                "                        \"relatedTicketDocumentNumber\": \"0745696282218\",\n" +
-                "                        \"relatedCouponNumber\": \"0\",\n" +
-                "                        \"serviceType\": \" \",\n" +
-                "                        \"reasonforIssuanceSubCode\": \"993\",\n" +
-                "                        \"feeOwnerAirlineDesignator\": \"MI\",\n" +
-                "                        \"consumedAtIssuance\": \" \",\n" +
-                "                        \"numberOfServices\": 1,\n" +
-                "                        \"industryCarrierIndicator\": \" \",\n" +
-                "                        \"remarks\": \"REBOOKING FEE INTERNATIONAL\"\n" +
-                "                    }\n" +
-                "                ]\n" +
-                "            },\n" +
-                "            \"dateOfIssuance\": \"20170801\",\n" +
-                "            \"timeOfIssuance\": \"16:11:00\",\n" +
-                "            \"automaticRepricingEngine\": \" \",\n" +
-                "            \"statisticalCode\": \"I\"\n" +
-                "        }\n" +
-                "    ],\n" +
-                "      \"reportedFormOfPayment\": [\n" +
-                "        {\n" +
-                "            \"amount\": 33600,\n" +
-                "            \"currency\": \"EUR2\",\n" +
-                "            \"type\": \"CA\",\n" +
-                "        \t\"address\": {\n" +
-                "            \"city\": \"Chania\",\n" +
-                "            \"countryCode\": \"GR\",\n" +
-                "            \"houseNumberOrName\": \"None\",\n" +
-                "            \"stateOrProvince\": \"\",\n" +
-                "            \"street\": \"L. Soudas\"\n" +
-                "          },\n" +
-                "          \"payer\": {\n" +
-                "            \"email\": \"string\",\n" +
-                "            \"firstName\": \"John\",\n" +
-                "            \"id\": \"string\",\n" +
-                "            \"lastName\": \"Doe\"\n" +
-                "          }\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"monetaryAmounts\": {\n" +
-                "    },\n" +
-                "    \"additionalInformation\": {\n" +
-                "        \"paymentInformationLine1\": \"CASH\",\n" +
-                "        \"paymentInformationLine2\": \"\"\n" +
-                "    },    \n" +
-                "    \"additionalSalesInformation\": [\n" +
-                "        {\n" +
-                "            \"fare\": \"EUR25.00\",\n" +
-                "            \"total\": \"EUR25.00\",\n" +
-                "            \"fareCalculationModeIndicator\": \"1\",\n" +
-                "            \"fareCalculationPricingIndactor\": \"1\",\n" +
-                "            \"fareCalculationArea1\": \"ZAG LX ZRH336HRK336END\"\n" +
-                "        }\n" +
-                "    ],\n" +
-                "    \"bookingInformation\": {}\n" +
-                "    }\n" +
-                "]        \n" +
-                "    ,\n" +
-                "    \"basicInformation\": {\n" +
-                "        \"countryCode\": \"FR\",\n" +
-                "        \"transactionCode\": \"EMDS\",\n" +
-                "        \"agentCode\": \"2021586\",\n" +
-                "        \"reasonForIssuanceCode\": \"C\",\n" +
-                "        \"channel\":\"NONGDS\",\n" +
-                "        \"clientID\": \"074\",\n" +
-                "        \"recordType\": \"EMD\",\n" +
-                "    \t\"pnrReport\": true\n" +
-                "    },\n" +
-                "    \"formOfPayment\": [\n" +
-                "        {\n" +
-                "          \"amount\": 33600,\n" +
-                "          \"currency\": \"EUR2\",\n" +
-                "          \"type\": \"CA\",\n" +
-                "          \"address\": {\n" +
-                "            \"city\": \"Chania\",\n" +
-                "            \"countryCode\": \"GR\",\n" +
-                "            \"houseNumberOrName\": \"None\",\n" +
-                "            \"stateOrProvince\": \"\",\n" +
-                "            \"street\": \"L. Soudas\"\n" +
-                "          },\n" +
-                "          \"payer\": {\n" +
-                "            \"email\": \"string\",\n" +
-                "            \"firstName\": \"John\",\n" +
-                "            \"id\": \"string\",\n" +
-                "            \"lastName\": \"Doe\"\n" +
-                "          }\n" +
-                "    }    \n" +
-                "  ]\n" +
+                " \"id\": \"string\",\n" +
                 "}";
     }
 
